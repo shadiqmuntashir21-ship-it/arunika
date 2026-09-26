a12ce9f836eec6d649033829c07febbed972873f
shadiqmuntashir21-ship-it <shadiqmuntashir21@gmail.com>
2026-09-26 09:40:40 +0800

    Refresh PWA cache for habit tracker update

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "arunika-v4-precision";
+const CACHE = "arunika-v5-habit-tracker";
 const CORE = ["/", "/app", "/pro", "/activate", "/manifest.webmanifest"];
 self.addEventListener("install", (event) => { event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE)).then(() => self.skipWaiting())); });
 self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())); });
