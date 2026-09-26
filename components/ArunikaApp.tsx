cc27491bafb602074532a3efd41ce87704b3518d
shadiqmuntashir21-ship-it <shadiqmuntashir21@gmail.com>
2026-09-26 18:52:41 +0800

    Pass Pro state to first-run tour welcome

--- components/ArunikaApp.tsx
+++ components/ArunikaApp.tsx
@@ -353,7 +353,7 @@ export function ArunikaApp() {
       <LearningForm open={learningModal} item={editingLearning} setItem={setEditingLearning} onClose={() => setLearningModal(false)} onSubmit={saveLearning} />
       <SessionForm open={sessionModal} session={editingSession} setSession={setEditingSession} books={snapshot.books.filter((b) => b.status !== "finished" || b.id === editingSession.bookId)} onClose={() => setSessionModal(false)} onSubmit={saveSession} />
       <LearningSessionForm open={learningSessionModal} session={editingLearningSession} setSession={setEditingLearningSession} items={snapshot.learning.filter((item) => item.status !== "finished" || item.id === editingLearningSession.learningId)} onClose={() => setLearningSessionModal(false)} onSubmit={saveLearningSession} />
-      <Onboarding open={onboarding} onTour={() => finishWelcome(true)} onSkip={() => finishWelcome(false)} />
+      <Onboarding open={onboarding} isPro={isPro} onTour={() => finishWelcome(true)} onSkip={() => finishWelcome(false)} />
       <ProductTour open={tourOpen} onClose={() => setTourOpen(false)} onNavigate={navigateTour} />
       {toast ? <div className="toast">{toast}</div> : null}
     </div>
